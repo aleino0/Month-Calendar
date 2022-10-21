afd93368188df67c2fa94ae7cd8233caae560ca5
aleino0 <113008053+aleino0@users.noreply.github.com>
2022-10-21 20:11:44 +0200

    Add files via upload

---
+++ MonthCalendarKontrola/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace monthkalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.moncalKalendar = new System.Windows.Forms.MonthCalendar();
+            this.btnTermin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // moncalKalendar
+            // 
+            this.moncalKalendar.Location = new System.Drawing.Point(47, 37);
+            this.moncalKalendar.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
+            this.moncalKalendar.MaxSelectionCount = 30;
+            this.moncalKalendar.Name = "moncalKalendar";
+            this.moncalKalendar.ShowWeekNumbers = true;
+            this.moncalKalendar.TabIndex = 0;
+            this.moncalKalendar.TabStop = false;
+            this.moncalKalendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.moncalKalendar_DateChanged);
+            // 
+            // btnTermin
+            // 
+            this.btnTermin.Location = new System.Drawing.Point(130, 281);
+            this.btnTermin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTermin.Name = "btnTermin";
+            this.btnTermin.Size = new System.Drawing.Size(100, 28);
+            this.btnTermin.TabIndex = 1;
+            this.btnTermin.Text = "Rezerviraj";
+            this.btnTermin.UseVisualStyleBackColor = true;
+            this.btnTermin.Click += new System.EventHandler(this.btnTermin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 340);
+            this.Controls.Add(this.btnTermin);
+            this.Controls.Add(this.moncalKalendar);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Upotreba MonthCalendar kontrole";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar moncalKalendar;
+        private System.Windows.Forms.Button btnTermin;
+    }
+}
+
---
+++ MonthCalendarKontrola/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace monthkalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTermin_Click(object sender, EventArgs e)
+        {
+            TimeSpan period = moncalKalendar.SelectionEnd
+                - moncalKalendar.SelectionStart;
+            if (period.Days < 1)
+            {
+
+                MessageBox.Show("Niste odabrali bla bla, Pritisnite SHIFT da odaberete bla bla bla"
+                    , "GREŠKA!",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            }
+
+            else
+            {
+                MessageBox.Show("Rezervirali ste period od " + moncalKalendar.SelectionStart.ToShortDateString() +
+                    " do " + moncalKalendar.SelectionEnd.ToShortDateString()
+                    + " dana", "Rezervacija",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            }
+        }
+
+        private void moncalKalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+    }
+}
